3ca5a58562b80f2224fedc2ccbdd682499d9079a
Marcel Ullrich <ullrich.marcel@web.de>
2020-07-13 21:27:36 +0200

    added tilesets

---
+++ TileSet/RPG.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.5" name="RPG" tilewidth="32" tileheight="32" tilecount="2744" columns="8">
+ <image source="rpg.png" width="256" height="11000"/>
+</tileset>
---
+++ TileSet/Tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.5" name="Tiles" tilewidth="32" tileheight="32" tilecount="128" columns="8">
+ <image source="A5_Tiles.png" width="256" height="512"/>
+</tileset>
